a6dd441864d20ba65c5052f277d0ae828c1536ba
EC2 Default User <ec2-user@ip-172-31-25-201.ec2.internal>
2021-09-24 01:04:17 +0000

    [Finish]

---
+++ lesson4.rb
@@ -0,0 +1,5 @@
+name = "阪野神純"
+puts name
+
+age = "22歳"
+puts age
---
+++ lesson6.rb
@@ -0,0 +1,8 @@
+total_price = 50
+if total_price > 100
+  puts "みかんを購入。所持金にあまりあり"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+else
+  puts "みかんを購入することが出来ません"
+end
